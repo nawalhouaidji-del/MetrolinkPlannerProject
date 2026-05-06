3943fa97868edeb54e578f60ebf6a172abc24e26
nawalhouaidji-del <nawalhouaidji@gmail.com>
2026-05-06 23:41:15 +0100

    Added working menu and station listing feature

--- Planner.java
+++ Planner.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*; 
 import java.nio.file.*;
 
-public class Planner {
+public class Planner {    
     public static void main(String[] args) {
 
         String filename = "Metrolink_times_linecolour(in).csv";
@@ -10,12 +10,18 @@ public class Planner {
         List<String> connections = new ArrayList<>(); //For storing connections
         Map<String, List<Connection>> graph = new HashMap<>(); //For storing the graph structure
 
+        //Scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        //User Welcome interface
+        System.out.println("Welcome to the Metrolink Planner");
+
         try{
 
             List<String> lines = Files.readAllLines(Paths.get(filename));
             String currentLine = ""; //Variable to remember which Metroline is currently being read
 
-            for (String line : lines) {
+            for (String line : lines) {                
                 line =line.trim(); //line name + removing extra spaces
                 
                 if (line.isEmpty()) continue; //Skipping empty lines
@@ -25,54 +31,89 @@ public class Planner {
                 if (parts[0].trim().equals("From") && parts[1].trim().equals("To")){ //Removing the spaces from the header row
                     continue; //Skiping the header line
                 }
+                
+                String firstPart = parts[0].trim(); //Getting the line name
+                String secondPart = (parts.length > 1) ? parts[1].trim() : ""; //Checks if there is a part after the line/place name
 
-                    String firstPart = parts[0].trim(); //Getting the line name
-                    String secondPart = (parts.length > 1) ? parts[1].trim() : ""; //Checks if there is a part after the line/place name
+                boolean isLineNameRow = (secondPart.isEmpty() && !firstPart.equals("From")); //Checking if the row is a line name
 
-                    boolean isLineNameRow = (secondPart.isEmpty() && !firstPart.equals("From")); //Checking if the row is a line name
-
-                    if (isLineNameRow){
-                        currentLine = firstPart;
-                        System.out.println("Now reading line: "+ currentLine);
-                        continue;
-                    }
-
-                    //Connection Row
-                    if (parts.length >= 3){
-                        //Setting up connection data from the parts array
-                        String fromStation = parts[0].trim();
-                        String toStation = parts[1].trim();
-                        String travelTimeStr = parts[2].trim();
-
-                        double travelTime = Double.parseDouble(travelTimeStr);
-
-                        //Adding station names to the set
-                        stationSet.add(fromStation);
-                        stationSet.add(toStation);
-
-                        //Storing the connection as a readable string
-                        connections.add(fromStation + "->" + toStation + " (" +travelTime + "mins) on" + currentLine);
-                        //Add connection in both directions
-                        graph.computeIfAbsent(fromStation, k -> new ArrayList<>()).add(new Connection(toStation, travelTime, currentLine));
-                    }
-
+                if (isLineNameRow){
+                    currentLine = firstPart;
+                    System.out.println("Now reading line: "+ currentLine);
+                    continue;
                 }
-            } catch (IOException e){
-                System.out.println("Error reading file" + e.getMessage());
+
+                //Connection Row
+                if (parts.length >= 3){
+                    //Setting up connection data from the parts array
+                    String fromStation = parts[0].trim();
+                    String toStation = parts[1].trim();
+                    String travelTimeStr = parts[2].trim();
+
+                    double travelTime = Double.parseDouble(travelTimeStr);
+
+                    //Adding station names to the set
+                    stationSet.add(fromStation);
+                    stationSet.add(toStation);
+
+                    //Storing the connection as a readable string
+                    connections.add(fromStation + "->" + toStation + " (" +travelTime + "mins) on" + currentLine);
+                    //Add connection in both directions
+                    graph.computeIfAbsent(fromStation, k -> new ArrayList<>()).add(new Connection(toStation, travelTime, currentLine));
+                    graph.computeIfAbsent(toStation, k -> new ArrayList<>()).add(new Connection(fromStation, travelTime, currentLine));
+                }    
+            }            
+            
+            //User Interface Menu
+            System.out.println("\nDate loaded Successfully!");
+            System.out.println("Found" + stationSet.size() + " stations in the network.\n");
+
+            while(true){
+                //User Service Choice
+                System.out.println("\n ~Main Menu~");
+                System.out.println("1. List all stations");
+                System.out.println("2. Plan a journey");
+                System.out.println("3.Exit");
+                System.out.println("Enter your choice (1-3): ");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                if (choice == 1){
+                    //Showing all stations
+                    System.out.println("\n~Stations~");
+                    List<String> sortedStations = new ArrayList<>(stationSet);
+                    Collections.sort(sortedStations);
+                    for (String station : sortedStations) {
+                        System.out.println(" -" + station);
+                    }
+                    System.out.println("Total: " + sortedStations.size() + " stations");
+                } else if (choice == 2){
+                    //Planning a journey
+                    System.out.println("Testing");
+                } else if (choice == 3){
+                    System.out.println("Thank you for using the Metrolink Planner. Goodbye!");
+                    break;
+                } else {
+                    System.out.println("Invalid choice. Please pick between (1-3)");
+                }
             }
+            scanner.close();
+                
+        } catch (IOException e){
+            System.out.println("Error reading file" + e.getMessage());
         }
     }
+}
+//Storing the information about a connection between two stations together
+class Connection {
+    String station;
+    double time;
+    String line;
 
-    //Storing the information about a connection between two stations together
-    class Connection {
-        String station;
-        double time;
-        String line;
+    Connection(String station, double time, String line) {
 
-        Connection(String station, double time, String line) {
-
-            this.station = station;
-            this.time = time;
-            this.line = line;
-        }
+        this.station = station;
+        this.time = time;
+        this.line = line;
     }
+}
